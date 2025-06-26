30ca81941d2e8fac84668e0eb47aa329fbefdaca
MATHEUS AUGUSTO CONCEICAO GUIMARAES <matheus.acguimaraes@senacsp.edu.br>
2025-06-26 15:02:23 -0300

    alterando projeto

--- Calculadora/frmCalculadora.Designer.cs
+++ Calculadora/frmCalculadora.Designer.cs
@@ -37,6 +37,7 @@
             this.lblResultado = new System.Windows.Forms.Label();
             this.lblResposta = new System.Windows.Forms.Label();
             this.btnLimpar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblNum1
@@ -122,11 +123,21 @@
             this.btnLimpar.UseVisualStyleBackColor = true;
             this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // frmCalculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(332, 362);
+            this.ClientSize = new System.Drawing.Size(605, 362);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.lblResposta);
             this.Controls.Add(this.lblResultado);
@@ -155,5 +166,6 @@
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Label lblResposta;
         private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button button1;
     }
 }
